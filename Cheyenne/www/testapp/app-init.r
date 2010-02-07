83a1938acbf4d60ebf00fc331981d11c12f745b9
Nenad Rakocevic <nenad@rakocevic.net>
2010-02-07 15:17:33 +0000

    FIX: valid code lines were wrongly commented.
    git-svn-id: http://cheyenne-server.googlecode.com/svn/trunk@70 ae0e6eb8-7602-11de-91f8-ab615253d9e8

--- Cheyenne/www/testapp/app-init.r
+++ Cheyenne/www/testapp/app-init.r
@@ -10,12 +10,13 @@ on-application-end: does [
 	;--- add here your library / modules proper closing
 ]
 
-on-instance-start: does [
+on-database-init: does [
 	;--- add here instance specific init code
 	;--- it's called when first database access is made using DO-SQL
 	;--- so typical usage is db-cache init when multiple instances
 	;--- of the same webapp are running on the same server.
 ]
+}
 
 on-session-start: does [
 	;--- add here your per session init code
@@ -30,7 +31,7 @@ on-session-end: does [
 	;--- add here your per session closing/cleanup code
 
 ]
-}
+
 on-page-start: has [][
 	set 't0 now/time/precise
 ]
