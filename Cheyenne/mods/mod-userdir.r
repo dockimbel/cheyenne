b2ccdc572bf6be683a694909e20166d9c1b44068
Nenad Rakocevic <nenad@rakocevic.net>
2010-09-24 19:15:37 +0000

    FIX: mod-userdir.r source code refactored and simplified
    git-svn-id: http://cheyenne-server.googlecode.com/svn/trunk@92 ae0e6eb8-7602-11de-91f8-ab615253d9e8

--- Cheyenne/mods/mod-userdir.r
+++ Cheyenne/mods/mod-userdir.r
@@ -1,6 +1,7 @@
 REBOL [
 	History: {
 		08/09/2010 - Applied Kaj's big patch 
+		24/09/2010 - Code refactored and simplified
 	}
 ]
 
@@ -8,89 +9,49 @@ install-HTTPd-extension [
 	name: 'mod-userdir
 	
 	order: none
-	lf: #"^/"
+	user: group: none
 	col: #":"
 	
-	boot-code: []
-	
-	on-started: does [do boot-code]
-	on-reload:  does [clear boot-code]
-	
-	get-ugid: func [name [string!] /local file line uid gid][
-		unless attempt [file: read/lines %/etc/passwd][
-			log/error "accessing /etc/passwd failed"
-			return none
+	on-started: does [
+		if all [user system/version/4 <> 3][		;-- exclude Windows
+			set-process-to user any [group user]
 		]
-		foreach line file [
-			if all [line: find/case/match line name  col = first line][
-				return either parse/all next line [
-					thru col
-					copy uid to col skip
-					copy gid to col
-					to end
-				][
-					reduce [to-integer uid to-integer gid]
-				][
-					log/error "invalid format reading /etc/passwd !"
-					none
-				]
+	]
+	on-reloaded: does [
+		user: group: none
+	]
+	
+	get-id: func [name [string!] /group /local file rule uid gid][
+		set [file rule] pick [
+			[%/etc/group  []]
+			[%/etc/passwd [copy uid to col skip]]	
+		] to-logic group
+
+		unless exists? file [log/error reform ["accessing" file "failed"]]
+		parse/case/all read file [
+			some [name col thru col rule copy gid to col break | thru newline]
+		]
+		unless any [[group gid] all [uid gid]][log/error reform ["id not found in" file]]
+		reduce [to-integer uid to-integer gid]
+	]
+
+	set-process-to: func [user [string! integer!] group [string! integer!] /local uid gid][
+		set [uid gid] reduce [user group]
+		if any [string? user string? group][
+			either user = group [
+				set [uid gid] get-id user
+			][
+				if string? user  [uid: first get-id user]
+				if string? group [gid: second get-id/group group]
 			]
 		]
-		log/error "user not found in /etc/passwd"
-		none
-	]
-	
-	get-gid: func [name [string!] /local file line gid][
-		unless attempt [file: read/lines %/etc/group][
-			log/error "accessing /etc/group failed"
-			return none
-		]
-		foreach line file [
-			if all [line: find/case/match line name  col = first line][
-				return either parse/all next line [
-					thru col
-					copy gid to col
-					to end
-				][
-					to-integer gid
-				][
-					log/error "invalid format reading /etc/group !"
-					none
-				]
-			]
-		]
-		log/error "group not found in /etc/group"
-		none
-	]
-	
-	change-id: func [id [string! integer!] /user /group /local gid][
-		either string? id [
-			unless id: get-ugid id [return none]
-			set [id gid] id
-		][
-			gid: id
-		]
-		if group [setgid gid]
-		if user [
-			;logger/file.log: join logger/file ["-" id %.log]
-			setuid id
-		]
-	]
-	
-	change-gid: func [id [string! integer!]][
-		if string? id [
-			unless id: get-gid id [return none]
-		]
-		setgid id
+		;-- change group id first to inherit privileges from group first
+		if not zero? setgid gid [log/error "setgid failed!"]
+		if not zero? setuid uid [log/error "setuid failed!"]
 	]
 	
 	words: [
-		user: [string! | integer!] in globals do [
-			repend boot-code either string? args/1 [['change-id/user/group args/1]] [['change-id/user args/1]]
-		]
-		group: [string! | integer!] in globals do [
-			unless empty? boot-code [change boot-code [change-id/user]]
-			insert boot-code reduce ['change-gid args/1]
-		]
+		user:  [string! | integer!] in globals do [user: args/1]
+		group: [string! | integer!] in globals do [group: args/1]
 	]
 ]
