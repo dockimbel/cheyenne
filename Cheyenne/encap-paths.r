d1014d9975f59239b4b2976361baacba9aafcee9
Nenad Rakocevic <nenad@rakocevic.net>
2011-04-18 10:22:53 +0000

    FIX: local SDK v2.7.8 paths fixed.
    git-svn-id: http://cheyenne-server.googlecode.com/svn/trunk@129 ae0e6eb8-7602-11de-91f8-ab615253d9e8

--- Cheyenne/encap-paths.r
+++ Cheyenne/encap-paths.r
@@ -2,19 +2,19 @@
 
 ;--- Windows include paths ---
 #if [system/version/4 = 3] [
-	#include %//dev/REBOL/SDK/Source/mezz.r
-	#include %//dev/REBOL/SDK/Source/prot.r
-	#include %//dev/REBOL/SDK/Source/gfx-colors.r
+	#include %//dev/SDK/v278/Source/mezz.r
+	#include %//dev/SDK/v278/Source/prot.r
+	#include %//dev/SDK/v278/Source/gfx-colors.r
 ]
 ;--- OS X include paths ---
 #if [system/version/4 = 2] [
-	#include %/Users/dk/dev/sdk/source/mezz.r
-	#include %/Users/dk/dev/sdk/source/prot.r
-	#include %/Users/dk/dev/sdk/source/gfx-colors.r
+	#include %/Users/dk/Desktop/sdk-278/source/mezz.r
+	#include %/Users/dk/Desktop/sdk-278/source/prot.r
+	#include %/Users/dk/Desktop/sdk-278/source/gfx-colors.r
 ]
 ;--- Linux include paths ---
 #if [system/version/4 = 4] [
-	#include %/root/Desktop/sdk/source/mezz.r
-	#include %/root/Desktop/sdk/source/prot.r
-	#include %/root/Desktop/sdk/source/gfx-colors.r
+	#include %/root/Bureau/sdk-v278/source/mezz.r
+	#include %/root/Bureau/sdk-v278/source/prot.r
+	#include %/root/Bureau/sdk-v278/source/gfx-colors.r
 ]
